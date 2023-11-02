ef393911a7a47977b1c15ec9a057d887fa7f7c10
SAI KUMAR <saikumar.marupakula1@gmail.com>
2023-11-01 21:32:20 -0500

    printing the string literal

--- demo.java
+++ demo.java
@@ -3,5 +3,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("Hi this is a git course");
         String a = "hi";
+        System.out.println(" a = "+a);
     }
 }
