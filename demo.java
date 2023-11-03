8a39a6b3dd85917d6cb129289bca17428eda3324
SAI KUMAR <saikumar.marupakula1@gmail.com>
2023-11-02 21:32:00 -0500

    code uupdated in feature/git-3

--- demo.java
+++ demo.java
@@ -3,7 +3,7 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("Hi this is a git course");
         String a = "hi";
-        System.out.println(" a = "+a); 
+        System.out.println(" a = "+a);  
         System.out.println("Updated code in feature/git-3");
         System.out.println("Updated code in feature/git");
         System.out.println("Updated in git 1 branch");
@@ -11,5 +11,6 @@ public class demo {
         System.out.println("Updated code in feature/git");
         System.out.println("Code updated in feature/git-2");
         System.out.println("Code updated in feature/git-2 again");
+        System.out.println("Updated code in feature/git-3");
     }
 }
