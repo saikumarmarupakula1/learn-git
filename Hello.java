838664ea8adc917a22cdf5d0c3937778c81c70be
SAI KUMAR <saikumar.marupakula1@gmail.com>
2023-11-08 22:24:21 -0600

    type conversion

--- Hello.java
+++ Hello.java
@@ -1,5 +1,18 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        byte b = 127;
+        System.out.println(b);
+        // byte c = 257;
+        // System.out.println(c);
+        int a = b;
+        System.out.println(a);
+        System.out.println("---------------");
+        int c = 12;
+        byte d = (byte)c;
+        System.out.println(d);
+        float f = 5.6f;
+        int g = (int)f;
+        System.out.println(g);
     }
 }
